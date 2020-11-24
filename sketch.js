a8e8678b22a181c36d8355822f3e10ac4565533d
Mukulika Sahoo <mukulika.kapoor@gmail.com>
2020-11-24 16:21:04 +0530

    added another pig

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1, pig2;
 var backgroundImg;
 
 function preload() {
@@ -26,6 +26,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
+    pig2 = new Pig(980, 220);
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -57,6 +58,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    pig2.display();
 
     bird.display();
 }
